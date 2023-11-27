95c00837d0b789cd6a2ea0837f8441df45287490
Ivan Koriakin <koryakin.ivan90@yandex.ru>
2023-11-27 19:54:21 +0700

    Сделана ДЗ по ООП №7 исправил

--- Seminar_OOP/src/HW/Seminar_7_HW/View/ViewCalculator.java
+++ Seminar_OOP/src/HW/Seminar_7_HW/View/ViewCalculator.java
@@ -4,6 +4,7 @@ import HW.Seminar_7_HW.Interface.Calculable;
 import HW.Seminar_7_HW.Interface.IComputingDevice;
 
 import java.util.Scanner;
+
 public class ViewCalculator {
     private IComputingDevice computingDevice;
 
@@ -12,35 +13,42 @@ public class ViewCalculator {
     }
 
     public void run() {
-        while (true) {
+        boolean exit = true;
+        while (exit) {
             Scanner scanner = new Scanner(System.in);
             System.out.print("Введите первый аргумент: ");
             if (scanner.hasNextFloat()){
                 float primaryArg = scanner.nextFloat();
                 Calculable calculator = computingDevice.create(primaryArg);
-                while (true) {
-                    String cmd = prompt("Введите команду (*, +, =) : ");
-                    if (cmd.equals("*")) {
-                        float arg = promptDouble("Введите второй аргумент: ");
-                        calculator.multi(arg);
-                        continue;
-                    }
-                    if (cmd.equals("+")) {
-                        float arg = promptDouble("Введите второй аргумент: ");
-                        calculator.sum(arg);
-                        continue;
-                    }
-                    if (cmd.equals("=")) {
-                        float result = calculator.getResult();
-                        System.out.println("Результат:" + result);
-                        break;
+                while (exit) {
+                    String cmd = prompt("Введите команду (+, -, *, /, =) : ");
+                    switch (cmd){
+                        case "*":
+                            float arg = promptDouble();
+                            calculator.multi(arg);
+                            break;
+                        case "+":
+                            float arg2 = promptDouble();
+                            calculator.sum(arg2);
+                            break;
+                        case "-":
+                            float arg3 = promptDouble();
+                            calculator.substr(arg3);
+                            break;
+                        case "/":
+                            float arg4 = promptDouble();
+                            calculator.separ(arg4);
+                            break;
+                        case "=":
+                            float result = calculator.getResult();
+                            System.out.println("Результат:" + result);
+                            exit = exitSwitch();
+                            if (exit){
+                                run();
+                            }
+                            break;
                     }
                 }
-                String cmd = prompt("Еще посчитать (Y/N)?");
-                if (cmd.equals("Y")) {
-                    continue;
-                }
-                break;
             }
             else {
                 System.out.println("Вы ввели не число!");
@@ -52,9 +60,20 @@ public class ViewCalculator {
         System.out.print(message);
         return in.nextLine();
     }
-    private float promptDouble(String message) {
+    private float promptDouble() {
         Scanner in = new Scanner(System.in);
-        System.out.print(message);
+        System.out.print("Введите следующий аргумент: ");
         return in.nextFloat();
+    }
+    private boolean exitSwitch(){
+        boolean exit = true;
+        String exitSwitch = prompt("Еще посчитать (y/n)?");
+        if (exitSwitch.equals("y")) {
+            return exit;
+        }
+        else {
+            exit = false;
+            return exit;
+        }
     }
 }
--- Seminar_OOP/src/HW/Seminar_7_HW/main.java
+++ Seminar_OOP/src/HW/Seminar_7_HW/main.java
@@ -2,23 +2,11 @@ package HW.Seminar_7_HW;
 
 import HW.Seminar_7_HW.Calculation.ComputingDevice;
 import HW.Seminar_7_HW.View.ViewCalculator;
-import HW.Seminar_7_HW.View.ViewCalculator2;
 
 public class main {
     public static void main(String[] args) {
         ComputingDevice computingDevice = new ComputingDevice();
-        ViewCalculator2 viewCalculator = new ViewCalculator2(computingDevice);
+        ViewCalculator viewCalculator = new ViewCalculator(computingDevice);
         viewCalculator.run();
-
-
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.print("Первое число: ");
-//        float num = scanner.nextFloat();
-//        Scanner scanner1 = new Scanner(System.in);
-//        System.out.print("Второе число: ");
-//        float num1 = scanner1.nextFloat();
-//        float res = num + num1;
-//        System.out.println(res);
-
     }
 }
